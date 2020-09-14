1083f368f5c0fdd25e521c98588c352362c4e50c
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2020-09-14 22:32:03 +0300

    Add files via upload

--- slider.js
+++ slider.js
@@ -16,7 +16,7 @@ slidersForCards.forEach(slider => {
     let horizontalScrollBar = slider.querySelector('.slider_for_cards_horizontal_scrollbar');
     let horizontalScrollThumb = slider.querySelector('.slider_for_cards_horizontal_scrollthumb');
     horizontalScrollBar.style.minWidth = `${visibleWidthOfSlider}px`;
-    horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 4}px`;
+    horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 8}px`;
     yearsBtns.forEach(btn => years.push(btn.textContent));
     currentYear.textContent = `${yearsBtns[indexOfActiveYear].textContent}`;
     let j = 0;
@@ -38,7 +38,7 @@ slidersForCards.forEach(slider => {
                     indexOfLeftSlide = cards.length - rightBorderOfSlider - 1;
                 }
                 cardsContainer.style.marginLeft = `${-marginLeftOfSlider}px`;
-                horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 4}px`;
+                horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 8}px`;
                 yearsBtns[i].classList.add('active_years');
                 yearsBtns[indexOfActiveYear].classList.remove('active_years');
                 indexOfActiveYear = i;
@@ -65,7 +65,7 @@ slidersForCards.forEach(slider => {
                 marginLeftOfSlider = cards.length * 600 - visibleWidthOfSlider + 100;
             }
             cardsContainer.style.marginLeft = `${-marginLeftOfSlider}px`;
-            horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 4}px`;
+            horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 8}px`;
             if (arrowRight.classList.contains('swiper-button-disabled')) {
                 arrowRight.classList.remove('swiper-button-disabled');
             }
@@ -88,7 +88,7 @@ slidersForCards.forEach(slider => {
                 marginLeftOfSlider = cards.length * 600 - visibleWidthOfSlider + 100;
             }
             cardsContainer.style.marginLeft = `${-marginLeftOfSlider}px`;
-            horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 4}px`;
+            horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 8}px`;
             if (arrowLeft.classList.contains('swiper-button-disabled')) {
                 arrowLeft.classList.remove('swiper-button-disabled');
             }
@@ -110,7 +110,7 @@ slidersForCards.forEach(slider => {
                 marginLeftOfSlider = cards.length * 600 - visibleWidthOfSlider + 100;
             }
             cardsContainer.style.marginLeft = `${-marginLeftOfSlider}px`;
-            horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 4}px`;
+            horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 8}px`;
             if (arrowRight.classList.contains('swiper-button-disabled')) {
                 arrowRight.classList.remove('swiper-button-disabled');
             }
@@ -130,7 +130,7 @@ slidersForCards.forEach(slider => {
                 marginLeftOfSlider = cards.length * 600 - visibleWidthOfSlider + 100;
             }
             cardsContainer.style.marginLeft = `${-marginLeftOfSlider}px`;
-            horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 4}px`;
+            horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 8}px`;
             if (arrowLeft.classList.contains('swiper-button-disabled')) {
                 arrowLeft.classList.remove('swiper-button-disabled');
             }
@@ -144,5 +144,48 @@ slidersForCards.forEach(slider => {
                 arrowRight.classList.add('swiper-button-disabled');
             }
         }
+    });
+    let thumbMoving = 0;
+    let elemLeft, x;
+    document.addEventListener('mousemove', (event) => {
+        if (thumbMoving) {
+            elemLeft = horizontalScrollBar.offsetLeft;
+            x = event.pageX - elemLeft;
+            if (x >= 0) {
+                horizontalScrollThumb.style.left = `${x - 8}px`;
+                marginLeftOfSlider = (x * (cards.length * 600 - visibleWidthOfSlider + 100) / visibleWidthOfSlider) - 8;
+                indexOfLeftSlide = Math.round(marginLeftOfSlider / 600);
+                cardsContainer.style.marginLeft = `${-marginLeftOfSlider}px`;
+            }
+            if (marginLeftOfSlider + visibleWidthOfSlider - 80 < indexOfBeginOfCardsEachYear[indexOfActiveYear] * 600) {
+                yearsBtns[indexOfActiveYear].classList.remove('active_years');
+                indexOfActiveYear--;
+                currentYear.textContent = `${yearsBtns[indexOfActiveYear].textContent}`;
+                yearsBtns[indexOfActiveYear].classList.add('active_years');
+            }
+            if (marginLeftOfSlider + visibleWidthOfSlider - 100 > indexOfBeginOfCardsEachYear[indexOfActiveYear + 1] * 600) {
+                yearsBtns[indexOfActiveYear].classList.remove('active_years');
+                indexOfActiveYear++;
+                currentYear.textContent = `${yearsBtns[indexOfActiveYear].textContent}`;
+                yearsBtns[indexOfActiveYear].classList.add('active_years');
+            }
+        }
+    });
+    horizontalScrollThumb.addEventListener('mousedown', (event) => {
+        cardsContainer.style.transition = 'margin-left 0s ease-in-out';
+        horizontalScrollThumb.style.transition = 'left 0s ease-in-out';
+        thumbMoving = 1;
+    });
+
+    document.addEventListener('mouseup', (event) => {
+        cardsContainer.style.transition = 'margin-left 0.6s ease-in-out';
+        horizontalScrollThumb.style.transition = 'left 0.6s ease-in-out';
+        thumbMoving = 0;
+        marginLeftOfSlider = indexOfLeftSlide * 600;
+        if (marginLeftOfSlider + visibleWidthOfSlider - 100 > cards.length * 600) {
+            marginLeftOfSlider = cards.length * 600 - visibleWidthOfSlider + 100;
+        }
+        cardsContainer.style.marginLeft = `${-marginLeftOfSlider}px`;
+        horizontalScrollThumb.style.left = `${marginLeftOfSlider * visibleWidthOfSlider / (cards.length * 600 - visibleWidthOfSlider + 100) - 8}px`;
     })
 })
